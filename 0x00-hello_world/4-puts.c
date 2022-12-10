0b3408892b76b741e396cdf3663219494a18f842
AliyuA5 <aliyubalaibrahim.2001@gmail.com>
2022-12-10 03:16:45 -0800

    programmes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry
+ *
  * Return: Always 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multi lingual puzzle;
-			return (0);
+	return (0);
 }
-gcc -c $CFILE
