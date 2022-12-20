372aefdbf16fdf9979fd84e427deded6692beef6
AliyuA5 <aliyubalaibrahim.2001@gmail.com>
2022-12-20 02:32:34 -0800

    go

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
  * main - reset_to_98 - update the value of a var using pointers
  * @n: int to update
  * Return: 0
- */
+*/
 
-int void reset_to_98(int *n)
+int void reset_to_98(int *n);
 {
 	*n = 98;
 }
